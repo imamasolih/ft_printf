d24e4fa4a0d8668abe2266fae18a292f54761048
imamasol <isk.mamasoliev@gmail.com>
2025-07-03 18:10:26 +0200

    function split

--- ft_paramint.c
+++ ft_paramint.c
@@ -5,8 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: imamasol <imamasol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/03 13:06:55 by imamasol          #+#    #+#             */
-/*   Updated: 2025/07/03 13:06:56 by imamasol         ###   ########.fr       */
+/*   Created: 2025/07/03 17:52:18 by imamasol          #+#    #+#             */
+/*   Updated: 2025/07/03 18:07:23 by imamasol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_paramint(int num, int *len)
+{
+	char *str;
+
+	str = ft_itoa(num);
+	if (str == NULL)
+	{
+		ft_putstr_fd("(null)", 1);
+		(*len) += 6;
+	}
+	else
+	{
+		ft_putstr_fd(str, 1);
+		(*len) += ft_strlen(str);
+		free(str);
+	}
+	return (2);
+}
--- ft_paramunt.c
+++ ft_paramunt.c
@@ -6,7 +6,24 @@
 /*   By: imamasol <imamasol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 13:07:08 by imamasol          #+#    #+#             */
-/*   Updated: 2025/07/03 13:07:09 by imamasol         ###   ########.fr       */
+/*   Updated: 2025/07/03 18:07:44 by imamasol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_paramunt(unsigned int num, int *len)
+{
+	char *str;
+	str = ft_utoa(num);
+	if (str == NULL)
+	{
+		ft_putstr_fd("(null)", 1);
+		(*len) += 6;
+	}
+	else
+	{
+		ft_putstr_fd(str, 1);
+		(*len) += ft_strlen(str);
+		free(str);
+	}
+	return (2);
+}
